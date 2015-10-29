78e2348350ff0327eecadd444ee11c495efca1e9
Devesh Bajpai <BAJDE02@ca.com>
2015-10-29 11:40:02 -0700

    Chocolate Feast Solution - updated

--- ChocolateFeast.py
+++ ChocolateFeast.py
@@ -1,11 +1,14 @@
 _author__ = 'deveshbajpai'
 
 '''
-Problem Url: https://www.hackerrank.com/challenges/find-digits
+Problem Url: https://www.hackerrank.com/challenges/chocolate-feast
 
-Algorithm: For each number, iterate over it's digits. This is done by doing a modulus with 10 to obtain the digit
-and then dividing it by 10 to move to next digit. It works from right to left. Check for each digit that it is firstly
-not zero, and divides completely the original number. Return the total count and the end of the loop.
+Algorithm: For the given n,c and m, firstly calculate the direct available choclates in chocs. Wrappers is equal to
+chocolates right now. Now, if the wrappers is more than equal to the discount count (m), get the current extra chocolates
+by dividing wrappers available with m. Increment the total extra count with this value (extra_new). Update the wrappers
+to be remaining still wrapped chocolates, which includes the modulus of wrappers with m and new extra chocolates (extra_new)
+Keep doing this till the count of wrapped chocolates is equal or greater than discount count (m).
+In the end, add the grand total of extra to the original chocolates count. Return this value as the final answer.
 '''
 
 def solve(n,c,m):
